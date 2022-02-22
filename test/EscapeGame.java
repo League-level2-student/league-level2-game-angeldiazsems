a3e02c98337e0e6f9052d0f1856afaddc6adb6b6
student <student@ip-172-31-56-50.us-west-2.compute.internal>
2022-02-21 18:11:03 -0800

    works

--- test/EscapeGame.java
+++ test/EscapeGame.java
@@ -1,19 +1,26 @@
 import javax.swing.JFrame;
-
-
 public class EscapeGame {
+	
 	public static void main(String[] args) {
 		EscapeGame escape = new EscapeGame();
 		escape.setup();
 	}
-
+	JFrame frame;
+	GamePanel panel;
+	
 	EscapeGame() {
-		JFrame frame = new JFrame();
-		GamePanel panel = new GamePanel();
-		frame.add(panel);
+		this.frame = new JFrame();
+		this.panel = new GamePanel();
 	}
 
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 500;
+	
+	
 	public void setup() {
 		frame.setVisible(true);
+		frame.setSize(WIDTH,HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(panel);
 	}
 }
--- test/GamePanel.java
+++ test/GamePanel.java
@@ -1,7 +1,40 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel {
+public class GamePanel implements KeyListener {
 
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
